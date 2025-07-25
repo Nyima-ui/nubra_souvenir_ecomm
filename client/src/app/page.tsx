9b9f4b89151d0a81af9a317f5181826023db0520
Nyima-ui <ntenzin492@gmail.com>
2025-07-25 22:00:35 +0530

    got the fonts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,33 @@
-
 export default function Home() {
   return (
-    <></>
+    <div className="text-3xl">
+      <h1 className="font-century font-normal">this is heading</h1>
+      <h1 className="font-century font-bold">this is heading</h1>
+
+      <p className="font-grotesk-200">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-300">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-400">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-500">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-600">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-700">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-800">
+        hello world, we will check your typeface
+      </p>
+      <p className="font-grotesk-900">
+        hello world, we will check your typeface
+      </p>
+    </div>
   );
 }
