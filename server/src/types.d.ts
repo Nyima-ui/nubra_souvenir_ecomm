e1e3a565058d3ca6699f8e40387b1bf61f42b5e8
Nyima-ui <ntenzin492@gmail.com>
2025-08-22 09:19:21 +0530

    rebuild @types/node

---
+++ server/src/types.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="node" />
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'test';
+      PORT?: string;
+      DATABASE_URL: string;
+      CLOUDINARY_CLOUD_NAME: string;
+      CLOUDINARY_API_KEY: string;
+      CLOUDINARY_API_SECRET: string;
+    }
+  }
+}
+
+export {};
